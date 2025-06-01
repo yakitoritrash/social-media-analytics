a03b99358fa2f34f582d59977d57adff18fb8add
Arch me <kishudwivedi30@gmail.com>
2025-06-01 23:12:42 +0530

    cassandra connected

--- auth-service/index.php
+++ auth-service/index.php
@@ -8,6 +8,8 @@ require __DIR__ . '/vendor/autoload.php';
 // Initialize App with PSR-7
 $app = AppFactory::create();
 
+$db = new PDO("mysql:host=localhost;dbname=social_auth", "auth_user", "password");
+
 //Test
 $app->get('/test', function (Request $request, Response $response) {
   $response->getBody()->write("Auth service running!");
