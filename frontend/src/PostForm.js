4be8aef8a30b01a9ba2ab0c60ed5c342ea344730
Arch me <kishudwivedi30@gmail.com>
2025-06-05 13:56:59 +0530

    postform

---
+++ frontend/src/PostForm.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { createPost } from "./api";
+
+export default function PostForm({ token, userId }) {
+  const [content, setContent] = useState("");
+
+  const handlePost = async() => {
+    try {
+      await createPost(token, { userId, content });
+      setContent("");
+      alert("Post created");
+    } catch (err) {
+      alert("Error creating post");
+    }
+  };
+
+  return (
+    <div>
+      <h3>Create Post</h3>
+      <textarea value={content} onChange={e => setContent(e.target.value)} />
+      <br />
+      <button onClick={handlePost}>Post</button>
+    </div>
+  );
+}
